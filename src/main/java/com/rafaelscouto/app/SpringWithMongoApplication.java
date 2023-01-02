58eb4123642ec8751daafa66a456660ae1447403
Rafael Couto <rafaelscouto@gmail.com>
2023-01-02 16:35:42 -0300

    First commit

---
+++ src/main/java/com/rafaelscouto/app/SpringWithMongoApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelscouto.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithMongoApplication.class, args);
+	}
+
+}
