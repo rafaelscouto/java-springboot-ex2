34f16effefd61ac0f109ef27b7b8d9f8ef40032c
Rafael Couto <rafaelscouto@gmail.com>
2023-01-03 18:03:54 -0300

    Insert user

--- src/main/java/com/rafaelscouto/app/services/UserService.java
+++ src/main/java/com/rafaelscouto/app/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafaelscouto.app.domain.User;
+import com.rafaelscouto.app.dto.UserDTO;
 import com.rafaelscouto.app.repository.UserRepository;
 import com.rafaelscouto.app.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
