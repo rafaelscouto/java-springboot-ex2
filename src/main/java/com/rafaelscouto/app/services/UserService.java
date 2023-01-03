8aa290caf4916be1b03b99461d76bfcde7296206
Rafael Couto <rafaelscouto@gmail.com>
2023-01-03 18:35:42 -0300

    Delete and Update user

--- src/main/java/com/rafaelscouto/app/services/UserService.java
+++ src/main/java/com/rafaelscouto/app/services/UserService.java
@@ -30,6 +30,22 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
