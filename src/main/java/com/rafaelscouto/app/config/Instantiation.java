d7fdba1f2fc4ee095f4a7cdf5886ce54937bf0b4
Rafael Couto <rafaelscouto@gmail.com>
2023-01-04 15:08:42 -0300

    Create AuthorDTO

--- src/main/java/com/rafaelscouto/app/config/Instantiation.java
+++ src/main/java/com/rafaelscouto/app/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.rafaelscouto.app.domain.Post;
 import com.rafaelscouto.app.domain.User;
+import com.rafaelscouto.app.dto.AuthorDTO;
 import com.rafaelscouto.app.repository.PostRepository;
 import com.rafaelscouto.app.repository.UserRepository;
 
@@ -35,11 +36,12 @@ public class Instantiation implements CommandLineRunner {
 		User u2 = new User(null, "Maria", "maria@teste.com.br");
 		User u3 = new User(null, "Jose", "jose@teste.com.br");
 		
-		Post p1 = new Post(null, sdf.parse("02/01/2023"), "Title 01", "Content 01", u1);
-		Post p2 = new Post(null, sdf.parse("03/01/2023"), "Title 02", "Content 02", u1);
-		Post p3 = new Post(null, sdf.parse("02/01/2023"), "Title 03", "Content 03", u2);
-
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+		
+		Post p1 = new Post(null, sdf.parse("02/01/2023"), "Title 01", "Content 01", new AuthorDTO(u1));
+		Post p2 = new Post(null, sdf.parse("03/01/2023"), "Title 02", "Content 02", new AuthorDTO(u1));
+		Post p3 = new Post(null, sdf.parse("02/01/2023"), "Title 03", "Content 03", new AuthorDTO(u2));
+		
 		postRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
